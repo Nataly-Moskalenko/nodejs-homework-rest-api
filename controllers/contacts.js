ac17186eabd626256a22df251673d92ea14b47d9
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-06-26 21:00:54 +0300

    fixes controllers and routers, adds updatestatuscontact-controller, adds router-patch

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,48 +1,58 @@
-const Contact = require('../models/contact');
+const { Contact } = require('../models/contact');
 
 const { HttpError, ctrlWrapper } = require('../helpers');
 
 const listContacts = async (req, res) => {
-  const allContacts = await Contact.find();
+  const allContacts = await Contact.find({}, '-createdAt -updatedAt');
   res.json(allContacts);
 };
 
 const getContactById = async (req, res) => {
   const id = req.params.contactId;
-  const contactById = await Contact.findOne({ id });
+  const contactById = await Contact.findById(id);
   if (!contactById) {
     throw HttpError(404, 'Not found');
   }
   res.json(contactById);
 };
 
-const addContact = async (req, res) => {  
+const addContact = async (req, res) => {
   const newContact = await Contact.create(req.body);
   res.status(201).json(newContact);
 };
 
 const removeContact = async (req, res) => {
   const id = req.params.contactId;
-  const removingContact = await Contact.findOneAndRemove({ id });
+  const removingContact = await Contact.findByIdAndRemove(id);
   if (!removingContact) {
     throw HttpError(404, 'Not found');
   }
   res.json({ message: 'Contact deleted' });
 };
 
-// const updateContact = async (req, res) => {
-//   const id = req.params.contactId;
-//   const updatingContact = await contacts.updateContact(id, req.body);
-//   if (!updatingContact) {
-//     throw HttpError(404, 'Not found');
-//   }
-//   res.json(updatingContact);
-// };
+const updateContact = async (req, res) => {
+  const id = req.params.contactId;
+  const updatingContact = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!updatingContact) {
+    throw HttpError(404, 'Not found');
+  }
+  res.json(updatingContact);
+};
+
+const updateStatusContact = async (req, res) => {
+  const id = req.params.contactId;
+  const updatingContact = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!updatingContact) {
+    throw HttpError(404, 'Not found');
+  }
+  res.json(updatingContact);
+};
 
 module.exports = {
   listContacts: ctrlWrapper(listContacts),
   getContactById: ctrlWrapper(getContactById),
   addContact: ctrlWrapper(addContact),
   removeContact: ctrlWrapper(removeContact),
-  // updateContact: ctrlWrapper(updateContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
--- models/contact.js
+++ models/contact.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require('mongoose');
 
+const Joi = require('joi');
+
 const { handleMongooseError } = require('../helpers');
 
 const contactSchema = new Schema(
@@ -24,6 +26,27 @@ const contactSchema = new Schema(
 
 contactSchema.post('save', handleMongooseError);
 
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean()
+    .required()
+    .error(new Error('Missing field favorite')),
+});
+
+const schema = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
 const Contact = model('contact', contactSchema);
 
-module.exports = Contact;
+module.exports = {
+  Contact,
+  schema,
+};
