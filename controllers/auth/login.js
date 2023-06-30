e37979a49ad8e2bf47f30c0c81aa1cc5d559ee49
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-06-30 14:09:33 +0300

    creates authenticate middleware

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,12 +1,12 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
 const { User } = require('../../models/user');
 
 const { HttpError } = require('../../helpers');
 
-// dotenv.config();
+dotenv.config();
 
 const { SECRET_KEY } = process.env;
 
@@ -16,25 +16,12 @@ const login = async (req, res) => {
   if (!user) {
     throw HttpError(401, 'Email or password invalid');
   }
-
   const comparePassword = await bcrypt.compare(password, user.password);
   if (!comparePassword) {
     throw HttpError(401, 'Email or password invalid');
   }
-
   const payload = { id: user.id };
-
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
-
-  // const decodeToken = jwt.decode(token);
-
-  // try {
-  //   const { id } = jwt.verify(token, SECRET_KEY);
-  //   console.log(id);
-  // } catch (error) {
-  //   console.log(error.message);
-  // }
-
   res.json({ token });
 };
 
