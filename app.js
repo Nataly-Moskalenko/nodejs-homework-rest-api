db8f30bf3c396d71ccdeb1762602ecc12d71b260
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-07-25 17:09:30 +0300

    adds swagger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 require('dotenv').config();
 
@@ -18,6 +20,7 @@ app.use(express.static('public'));
 
 app.use('/api/users', authRouter);
 app.use('/api/contacts', contactsRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
